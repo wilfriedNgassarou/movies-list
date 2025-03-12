d342d131e9eb3910aff125c384e9c71cfef8734b
unknown <wilfriedngassarou23@gmail.com>
2025-03-12 11:51:20 +0100

    Added credits

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Credits } from "./components/credits"
 import { MovieItem } from "./components/movie"
 import { movies } from "./constants/movies"
 
 function App() {
   return (
     <section className="w-full h-dvh flex gap-4 justify-center items-center">
+      <Credits />
       <div className="fixed inset-0 lg:hidden z-10 bg-white flex justify-center items-center">
         <span className="text-lg">
           Please, use a large screen !
